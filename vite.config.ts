2a7c2371e7a9880b25a6086c0e65dd4a4d2c86e9
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2025-03-21 13:25:04 +0100

    fix names and exports

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, 'src/main.ts'), formats: ['es'] } },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      fileName: 'main',
+      formats: ['es'],
+    },
+  },
   resolve: { alias: { src: resolve('src/') } },
   plugins: [dts({ exclude: '**/*.test.ts' })],
 });
